c86bc75cad550a704de9a231940c995e3e5a1bb7
Jose Luis Lopez <joseluis.lopez@webfg.com>
2016-08-17 15:49:49 +0200

    "seguridad y otros cierre sesion navegador"

--- resources/enter.php
+++ resources/enter.php
@@ -1,10 +1,22 @@
 <?
+
+//si es necesario cambiar la config. del php.ini desde tu script
+ini_set("session.use_only_cookies","1");
+ini_set("session.use_trans_sid","0");
+
 //vemos si el usuario y contraseña es váildo
 if ($_POST["user"]=="lobogriscreativo" && $_POST["password"]=="PandorA976"){
     //usuario y contraseña válidos
-    //defino una sesion y guardo datos
+    session_name("usuariologueado");
+    //asigno un nombre a la sesión para poder guardar diferentes datos
     session_start();
+    // inicio la sesión
+    session_set_cookie_params(0, "/", $HTTP_SERVER_VARS["HTTP_HOST"], 0);
+    //cambiamos la duración a la cookie de la sesión
     $_SESSION["autentificado"]= "SI";
+    //defino la sesión que demuestra que el usuario está autorizado
+    $_SESSION["lastacces"]= date("Y-n-j H:i:s");
+    //defino la fecha y hora de inicio de sesión en formato aaaa-mm-dd hh:mm:ss
     header ("Location: section/data.php");
 }else {
     //si no existe le mando otra vez a la portada
--- resources/sec.php
+++ resources/sec.php
@@ -1,12 +1,33 @@
 <?
-//Inicio la sesión
-session_start();
+//si es necesario cambiar la config. del php.ini desde tu script
+ini_set("session.use_only_cookies","1");
+ini_set("session.use_trans_sid","0");
 
-//COMPRUEBA QUE EL USUARIO ESTA AUTENTIFICADO
+//iniciamos la sesión
+session_name("usuariologueado");
+session_start();
+session_set_cookie_params(0, "/", $HTTP_SERVER_VARS["HTTP_HOST"], 0);
+//cambiamos la duración a la cookie de la sesión
+
+//antes de hacer los cálculos, compruebo que el usuario está logueado
+//utilizamos el mismo script que antes
 if ($_SESSION["autentificado"] != "SI") {
-    //si no existe, envio a la página de autentificacion
+    //si no está logueado lo envío a la página de autentificación
     header("Location: ../../index.php");
-    //ademas salgo de este script
-    exit();
+} else {
+    //sino, calculamos el tiempo transcurrido
+    $fechaGuardada = $_SESSION["lastacces"];
+    $ahora = date("Y-n-j H:i:s");
+    $tiempo_transcurrido = (strtotime($ahora)-strtotime($fechaGuardada));
+
+    //comparamos el tiempo transcurrido
+    if($tiempo_transcurrido >= 600) {
+        //si pasaron 10 minutos o más
+        session_destroy(); // destruyo la sesión
+        header("Location: ../../index.php"); //envío al usuario a la pag. de autenticación
+        //sino, actualizo la fecha de la sesión
+    }else {
+        $_SESSION["lastacces"] = $ahora;
+    }
 }
 ?>
